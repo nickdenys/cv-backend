ff768f64edb2b27a942d81b8d3b4ac79050b4068
Nick Denys <nickdenys@gmail.com>
2025-09-25 20:27:35 +0200

    fix: optimize imports

--- app/Http/Controllers/Api/V1/ProjectController.php
+++ app/Http/Controllers/Api/V1/ProjectController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ProjectResource;
 use App\Models\Project;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
